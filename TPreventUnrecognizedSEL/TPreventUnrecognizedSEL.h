20cb933e856687946eb08cac29b9e5d3e75a8cdf
TBD <weinanshao@163.com>
2017-11-23 22:52:03 +0800

    Initial Commit

---
+++ TPreventUnrecognizedSEL/TPreventUnrecognizedSEL.h
@@ -0,0 +1,19 @@
+//
+//  TPreventUnrecognizedSEL.h
+//  TPreventUnrecognizedSEL
+//
+//  Created by TBD on 2017/11/23.
+//  Copyright © 2017年 TBD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPreventUnrecognizedSEL.
+FOUNDATION_EXPORT double TPreventUnrecognizedSELVersionNumber;
+
+//! Project version string for TPreventUnrecognizedSEL.
+FOUNDATION_EXPORT const unsigned char TPreventUnrecognizedSELVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPreventUnrecognizedSEL/PublicHeader.h>
+
+
